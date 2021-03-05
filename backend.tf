d978e301575dda251031a496f8de3beb55043187
Arun M <marunk20@ford.com>
2021-03-05 20:21:06 +0530

    Backend added

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
-/*terraform {
+terraform {
   backend "gcs" {
     bucket  = "lateral-insight-188914.appspot.com"
     #prefix  = "marunk20/dev" // Not required here. This should go in PARTIAL_CONFIGURATION
   }
-}*/
+}
